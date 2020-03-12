4a6bcf2d33d3321634c03cc485c7948e5ae7cc55
karapv <andrewkarapenkov@gmail.com>
2020-03-12 20:37:30 +0200

    Add task 3

--- Tasks for js/task3/script.js
+++ Tasks for js/task3/script.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded',function () {
-   const URLtoObj = (url) =>{
-       let currentUrl = url.match(/\?.*/g).join('');
-   }
+    //Функция по преобразованию гет параметров с текущей страницы в объект
+    const URLtoObj = () =>{
+        let currentUrl = window.location.href.match(/\?(.*)/g).join('')
+            .replace(/&/g,',')
+            .replace(/\?/,'')
+            .replace(/=/g,':')
+            .replace(/([a-z]+)(:)/g,str=>`"${str.replace(/:/g,'":')}`)
+            .replace(/:([a-zA-Z]+)/g,str=>`${str.replace(/:/g,':"')}"`);
+        currentUrl = `{${currentUrl}}`;
+        currentUrl = eval('('+currentUrl+')');
+        return currentUrl;
+    };
+    console.log(URLtoObj());
 });
